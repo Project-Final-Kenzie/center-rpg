31631b98c2a0d7b93d765765f1ce13a237acc0eb
Samuel D' Siqueira <samueldsiqueira@gmail.com>
2022-11-03 19:00:06 -0300

    style: :art: change backgrond register and remove padding
    change backgrond register and remove padding div display

--- src/pages/Regiter/style.register.ts
+++ src/pages/Regiter/style.register.ts
@@ -1,19 +1,14 @@
-import styled from "styled-components";
-import background from "../../assets/img/backgroud-register.svg"
+import styled from 'styled-components';
+import background from '../../assets/img/background.svg';
 
 export const Container = styled.main`
-
-    display: flex;
-    justify-content: center;
-    padding: 100px 0 0 0; 
-    background-image: url(${background});
-    -webkit-background-size: cover;
-    -o-background-size: cover;
-    -moz-background-size: cover;
-    background-size: cover;
-
-    height: 100vh;
-
-
-`
-
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-image: url(${background});
+	-webkit-background-size: cover;
+	-o-background-size: cover;
+	-moz-background-size: cover;
+	background-size: cover;
+	height: 100vh;
+`;
