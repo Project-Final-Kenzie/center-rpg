12b2e99bdd4d01bf93589229228fea76f581d7a6
Paulo Vitor <patoassado@gmail.com>
2022-11-07 11:31:40 -0300

    fix: home/dashboard

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,47 +1,31 @@
 import { UserHistoriesContext } from "../../contexts/HistoriesContext";
 import { IhistoriesData } from "../../interface/typeHistories";
-import { useContext } from "react";
-import { UserContext } from "../../contexts/AuthContext";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
 
 const Home = () => {
   const { historiesData, loading } = UserHistoriesContext();
-  const navigate = useNavigate();
-  const { userState } = useContext(UserContext);
-
-  useEffect(() => {
-    !localStorage.getItem("@TOKEN") && navigate("/login");
-  }, []);
 
   return (
     <>
-      {!userState ? (
-        <></>
-      ) : (
-        <>
-          <header>
-            <img src="" alt="logotipo" />
-            <button>Login</button>
-          </header>
-          <div>
-            <ul>
-              {historiesData?.map((historie: IhistoriesData) => {
-                return (
-                  <li key={`${historie.id}`}>
-                    <img src={historie.photo} alt={historie.title} />
-                    <div>
-                      <p>{historie.title}</p>
-                      <p>{historie.description}</p>
-                      <button>Ver Mais</button>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        </>
-      )}
+      <header>
+        <img src="" alt="logotipo" />
+        <button>Login</button>
+      </header>
+      <div>
+        <ul>
+          {historiesData?.map((historie: IhistoriesData) => {
+            return (
+              <li key={`${historie.id}`}>
+                <img src={historie.photo} alt={historie.title} />
+                <div>
+                  <p>{historie.title}</p>
+                  <p>{historie.description}</p>
+                  <button>Ver Mais</button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
