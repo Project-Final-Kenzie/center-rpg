23a562c664db15daad989b182ec846b0fd303b49
Samuel D' Siqueira <samueldsiqueira@gmail.com>
2022-11-09 12:07:16 -0300

    style: :lipstick: remove header of main container
    remove header of main container

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,119 +1,53 @@
-import { StyledHeader } from "../../components/Header/styled.header";
-import { History, LinkStyled, StyledContainer } from "./styled.home";
+import { StyledHeader } from '../../components/Header/styled.header';
+import { History, LinkStyled, StyledContainer } from './styled.home';
+import { historiesData } from '../../constants/histories';
 
 //import { UserHistoriesContext } from "../../contexts/HistoriesContext";
 //import { IhistoriesData } from "../../interface/typeHistories";
 
-import logo from "../../assets/img/d20.svg";
-import { Carousel } from "../../components/carousel";
+import logo from '../../assets/img/d20.svg';
+import { Carousel } from '../../components/carousel';
 
 const Home = () => {
-  //const { historiesData, loading } = UserHistoriesContext();
+	//const { historiesData, loading } = UserHistoriesContext();
 
-  /*if (!loading) {
+	/*if (!loading) {
     return <p>Carregando...</p>;
   }
   Comentado para poder estilizar o retorno abaixo
   */
 
-  let historiesData = [
-    {
-      id: 1,
-      createdAt: "2021-03-31T15:46:05.000Z",
-      updatedAt: "2021-03-31T15:46:05.000Z",
-      owner: 1,
-      title: "História 1",
-      description:
-        "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      photo:
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
-      content: "Conteúdo da história 1",
-      views: 0,
-      comments: [],
-      members: [],
-      stars: 0,
-    },
-    {
-      id: 2,
-      createdAt: "2021-03-31T15:46:05.000Z",
-      updatedAt: "2021-03-31T15:46:05.000Z",
-      owner: 1,
-      title: "História 1",
-      description:
-        "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      photo:
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
-      content: "Conteúdo da história 1",
-      views: 0,
-      comments: [],
-      members: [],
-      stars: 0,
-    },
-    {
-      id: 3,
-      createdAt: "2021-03-31T15:46:05.000Z",
-      updatedAt: "2021-03-31T15:46:05.000Z",
-      owner: 1,
-      title: "História 1",
-      description:
-        "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      photo:
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
-      content: "Conteúdo da história 1",
-      views: 0,
-      comments: [],
-      members: [],
-      stars: 0,
-    },
-    {
-      id: 4,
-      createdAt: "2021-03-31T15:46:05.000Z",
-      updatedAt: "2021-03-31T15:46:05.000Z",
-      owner: 1,
-      title: "História 1",
-      description:
-        "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      photo:
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
-      content: "Conteúdo da história 1",
-      views: 0,
-      comments: [],
-      members: [],
-      stars: 0,
-    },
-  ];
+	return (
+		<>
+			<StyledHeader>
+				<div>
+					<img src={logo} alt='logotipo' />
+					<LinkStyled to={'/login'}>Login</LinkStyled>
+				</div>
+			</StyledHeader>
 
-  return (
-    <>
-      <StyledContainer>
-        <StyledHeader>
-          <div>
-            <img src={logo} alt="logotipo" />
-            <LinkStyled to={"/login"}>Login</LinkStyled>
-          </div>
-        </StyledHeader>
+			<StyledContainer>
+				<Carousel />
 
-        <Carousel />
+				<ul>
+					{historiesData?.map((h /*: IhistoriesData*/) => {
+						return (
+							<History key={`${h.id}`}>
+								<img src={h.photo} alt={h.title} />
 
-        <ul>
-          {historiesData?.map((h /*: IhistoriesData*/) => {
-            return (
-              <History key={`${h.id}`}>
-                <img src={h.photo} alt={h.title} />
+								<p>{h.title}</p>
 
-                <p>{h.title}</p>
-
-                <div>
-                  <span>{h.description}</span>
-                </div>
-                <button>Ver Mais</button>
-              </History>
-            );
-          })}
-        </ul>
-      </StyledContainer>
-    </>
-  );
+								<div>
+									<span>{h.description}</span>
+								</div>
+								<button>Ver Mais</button>
+							</History>
+						);
+					})}
+				</ul>
+			</StyledContainer>
+		</>
+	);
 };
 
 export default Home;
