e0748499f867027c7d35bc9c53b97cc1cdacd3e8
Silas Junior <silas.mcoach@gmail.com>
2022-11-09 22:24:17 -0300

    feat: create page Campaign style

--- src/pages/Campaign/index.tsx
+++ src/pages/Campaign/index.tsx
@@ -1,69 +1,108 @@
+import { StyledHeader } from '../../components/Header/styled.header';
+import * as S from './style.campaign';
+import logo from "../../assets/img/d20.svg";
+
+
 export const Compaings = () => {
     return(
-        <>
-        <div>
-            <h1>Seja bem vindo a campanha</h1>
-            <h3>{campanha.title}</h3>
-        </div>
-        <div>
-            <div>
-                <img src='' alt=''></img>
-            </div>
-            <div>
-                <p>Dados do Mestre</p>
-                <p>Nome: {owner.name}</p>
-                <p>Email: {onwer.email}</p>
-            </div>
-        </div>
-        <div>
-            <p>Duração da Campanha: {campanha.duration} </p>
-            <p>Ambienteção: {campanha.ambientation}</p>
-            <p>Discord da Capanha: <Link>Clique Aqui</Link></p>
-        </div>
-        <div>
-            <button>Ingressar nessa campanha</button>
-        </div>
+        <S.DivBody>
+            <StyledHeader>
+                <nav>
+                    <div>
+                        <img src={logo} alt="Central RPG" />
+                        <S.LinkStyledButton to={"/dashboard"}>Voltar</S.LinkStyledButton>
+                    </div>
+                </nav>
+            </StyledHeader>
 
-        <div>
-            <h1>Membros da Campanha</h1>
-            <div>
-                <ul>
-                    <li>
-                        <div>
-                            <p>Nome: </p>
-                            <p>Nivel: </p>
-                        </div>
-                        <div>
-                        <button></button>
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <p>Nome: </p>
-                            <p>Nivel: </p>
-                        </div>
-                        <div>
-                        <button></button>
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <p>Nome: </p>
-                            <p>Nivel: </p>
-                        </div>
-                        <div>
-                        <button></button>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div>
-            <h1>História</h1>
-            <div>
-                <p>campanha.content</p>
-            </div>
-        </div>
-        </>
+            <S.DivMain>
+
+                <S.DivTitle>
+                    <h1>Seja bem vindo a campanha</h1>
+                   
+                    <h3>Nome da campanha aqui</h3>
+                </S.DivTitle>
+
+                <S.DivContainer>
+
+                    <S.SideOne>
+
+                        <S.DivMaster>
+                            <div className='sideOne'>
+                                <img src={logo} alt=''></img>
+                            </div>
+                            <div className='sideTwo'>
+                                <h1>Dados do Mestre</h1>
+                               
+                                <p>Nome: Nome Aqui</p>        
+                                <p>Email: Email aqui</p>
+                            </div>
+                        </S.DivMaster>
+                        
+                        <S.DivData>
+                            <div className='dataStyle'>
+                                <h1>Duração da Campanha:</h1>
+                                <p>Lógica Aqui</p>
+                            </div>
+                            <div className='dataStyle'>
+                                <h1>Ambienteção:</h1>
+                                <p>Lgica Aqui</p>ó
+                            </div>
+                            <div className='dataStyleTwo'>
+                                <h1>Discord da Capanha:</h1>
+                                <S.LinkStyled to={"/teste"}>Lógica Aqui</S.LinkStyled>
+                            </div>
+                        </S.DivData>
+                        <S.DivButton>
+                            <button>Ingressar nessa campanha</button>
+                        </S.DivButton>
+                        <S.DivUsers>
+                            <div className='DivTitleUser'>
+                                <h1>Membros da Campanha</h1>
+                            </div>
+
+                            <div className='container'>
+                                <ul>
+                                    <li>
+                                        <div className='sideOne'>
+                                            <p>Nome: Nome Aqui </p>
+                                            <p>Nivel: Nível aqui</p>
+                                        </div>
+                                        <div className='sideTwo'>
+                                        <button>X</button>
+                                        </div>
+                                    </li>
+                                </ul>
+
+                                <ul>
+                                    <li>
+                                        <div className='sideOne'>
+                                            <p>Nome: Nome Aqui </p>
+                                            <p>Nivel: Nível aqui</p>
+                                        </div>
+                                        <div className='sideTwo'>
+                                        <button>X</button>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </S.DivUsers>
+
+                    </S.SideOne>
+                    
+                    <S.SideTwo>
+
+                        <S.DivHistory>
+                            <h1>História</h1>
+                            <p>testestestestestes</p>
+                        </S.DivHistory>
+
+                    </S.SideTwo>
+
+                </S.DivContainer>
+
+            </S.DivMain>
+
+        </S.DivBody>
     )
 }
---
+++ src/pages/Campaign/style.campaign.ts
@@ -0,0 +1,278 @@
+import styled from 'styled-components';
+import { Link } from "react-router-dom";
+import background from '../../assets/img/imagem-5.jpg'
+
+
+export const DivBody = styled.body`
+    width: 100%;
+    height: 100vh;
+    background-image: url(${background});
+    -webkit-background-size: cover;
+	-o-background-size: cover;
+	-moz-background-size: cover;
+	background-size: cover;
+
+`
+
+export const DivMain = styled.main`
+
+    width: 80%;
+    height: 85%;
+    background-color: ${({ theme }) => theme.colors.rgb.quartiary};
+    border-radius: 80px 8px 80px 8px;
+    margin: 50px auto;
+    padding: 20px;
+    
+`
+
+export const DivContainer = styled.div`
+    display: flex;
+    width: 100%;
+    gap: 25px;
+
+`
+
+export const SideOne = styled.div`
+   
+   width: 45%;
+
+`
+
+export const SideTwo = styled.div`
+    width: 45%;
+`
+
+export const LinkStyledButton = styled(Link)`
+
+  width: 98px;
+  height: 37px;
+  background: ${({ theme }) => theme.colors.white.primary};
+  box-shadow: 0px 4px 4px ${({ theme }) => theme.colors.rgb.tertiary};
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 16px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: ${({ theme }) => theme.colors.gray.quartiary};
+  text-decoration: none;
+
+`
+
+export const LinkStyled = styled(Link)`
+
+    font-size: 16px;
+    color: ${({ theme }) => theme.colors.yellow.secondary};
+
+`
+
+export const DivTitle = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    h1{
+        font-size: 25px;
+        color: ${({ theme }) => theme.colors.white.primary};
+    }
+
+    h3{
+        font-size: 18px;
+        color: ${({ theme }) => theme.colors.yellow.secondary};
+    }
+`
+
+export const DivMaster = styled.div`
+
+    border: 1px solid ${({ theme }) => theme.colors.white.primary};
+    
+    display: flex;
+    border-radius: 80px 8px 80px 8px;
+    padding: 10px;
+    gap: 10px;
+    margin-top: 20px;
+
+    .sideOne{
+        
+        background-color: white;
+        border-radius: 80px 8px 80px 8px;
+        img{
+            width: 150px;
+            height: 150px;
+            border-radius: 80px 8px 80px 8px;
+        }
+        
+    }
+
+    .sideTwo{
+
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+        margin-top: 45px;
+
+        h1{
+            font-size: 18px;
+            color: ${({ theme }) => theme.colors.white.primary};
+        }
+
+        p{
+            font-size: 16px;
+            color: ${({ theme }) => theme.colors.white.primary};
+            font-weight: 200;
+        }
+    }
+    
+
+`
+
+export const DivData = styled.div`
+
+ display: flex;
+ flex-direction: column;
+ gap: 10px;
+ margin-top: 20px;
+
+.dataStyle{
+
+    display: flex;
+    gap: 5px;
+
+    h1{
+        font-size: 18px;
+        color: ${({ theme }) => theme.colors.white.primary};
+    }
+
+    p{
+
+        font-size: 16px;
+        color: ${({ theme }) => theme.colors.yellow.secondary};
+
+    }
+
+}
+
+.dataStyleTwo{
+
+    display: flex;
+    gap: 5px;
+
+    h1{
+        font-size: 18px;
+        color: ${({ theme }) => theme.colors.white.primary};
+    }
+
+}
+
+`
+
+export const DivButton = styled.div`
+
+    margin-top: 30px;
+    margin-left: 30%;
+
+    button{
+        background-color: ${({ theme }) => theme.colors.yellow.quartiary};
+        width: 250px;
+        height: 45px;
+        border-radius: 8px;
+        font-size: 16px;
+        font-weight: 700;
+        color: ${({ theme }) => theme.colors.gray.secondary};
+    }
+
+`
+
+export const DivUsers = styled.div`
+
+    .DivTitleUser{
+        
+            font-size: 12px;
+            color: ${({ theme }) => theme.colors.white.primary};
+            margin-left: 28%;
+            margin-top: 10px;
+    }
+
+
+    .container{
+
+        border: 1px solid ${({ theme }) => theme.colors.white.primary};
+        height: 230px;
+        display: flex;
+        flex-direction: column;
+        border-radius: 80px 8px 80px 8px;
+        padding: 10px;
+        gap: 10px;
+        margin-top: 10px;
+        
+        ul{
+            background-color: ${({ theme }) => theme.colors.gray.primary};
+            width: 400px;
+            padding: 15px;
+            border-radius: 8px;
+            margin: 0 auto;
+        }
+        li{
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+
+        p{
+
+            font-size: 12px;
+            color: ${({ theme }) => theme.colors.white.primary};
+            margin-left: 10px;
+            margin-bottom: 5px;
+
+        }
+
+        button{
+            background-color: ${({ theme }) => theme.colors.gray.tertiary};
+            width: 25px;
+            height: 25px;
+            margin-bottom: 20px;
+            border-radius: 4px;
+            font-size: 16px;
+            font-weight: 700;
+            color: ${({ theme }) => theme.colors.gray.primary};
+        }
+    }
+
+
+
+`
+
+export const DivHistory = styled.div`
+
+    h1{
+        font-size: 35px;
+        color: ${({ theme }) => theme.colors.white.primary};
+        width: 100%;
+        text-align: center;
+        
+    }
+
+    
+    p{
+        background-color: ${({ theme }) => theme.colors.white.primary};
+        height: 70vh;
+        border-radius: 80px 8px 80px 8px;
+        font-size: 14px;
+        padding: 50px;
+        
+    }
+   
+
+
+
+`
